b16e713c394439b8f489d14ba2d750e363ca6288
Melissa Zhu <zhum17@cardiff.ac.uk>
2023-07-19 09:42:14 +0100

    Added text analysis function

--- app.js
+++ app.js
@@ -4,16 +4,25 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const unirest = require("unirest");
 const axios = require('axios');
-const nlp = require('compromise')
+const { Configuration, OpenAIApi } = require("openai");
 
 // Set up Express framework
 const app = express();
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 
-// Add API key 
-const API_KEY = process.env.RANDOMER_API_TOKEN;
+// Configure OpenAI 
+const configuration = new Configuration({
+    apiKey: process.env.OPEN_AI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// API key for Google search
+const GOOGLE_KEY = process.env.GOOGLE_KEY;
 
 // Function to get gender of a given name
 const getGender = async (name) => {
@@ -33,18 +42,80 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
+app.post('/detect', async (req, res) => {
+    // Obtain the text to analyze from the request body
+    let article_text = req.body.article_text;
+    
+    let perspectives_data = [];
+    try {
+        const response = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {role: "system", content: `Given the text below, identify the individuals who are directly quoted in the text. IMPORTANT: Do not include the main newsmakers or subjects of the story, only those providing additional opinions. For each individual, provide their name, likely gender, and a description of their role in the story, which could be used to search for a similar individual in a search engine or LinkedIn. Where pronouns or honorifics indicating gender are used, state the gender accordingly (e.g. male, female or non-binary). Otherwise, make a guess based on the name or other contextual clues available. /n/n For the role, focus on aspects of their background or expertise that are relevant to the story. For example, if a person is quoted because they live in an area that is the subject of the story, their profession is not as important as the fact that they are a resident of the area. Avoid mentioning the company the person works at unless it is relevant to the story, as it is more likely that their professional expertise is why they were quoted, and people with similar expertise may not work at the same company. /n/n Return your response as an array of JavaScript object, with each object representing an individual. For example, if the three people contributing opinions to the article are "Tom Holland, senior political analyst at think tank XYZ", "Angeline Baker, marketing manager and resident of Cardiff city" and "Richard Tate, owner of local business The Beaver's Den", the response could be something like this:
+                [
+                    {
+                        "name": "Tom Holland",
+                        "gender": "male",
+                        "role": "political analyst at a think tank"
+                    },
+                    {
+                        "name": "Angeline Baker",
+                        "gender": "female",
+                        "profession": "resident of Cardiff city"
+                    },
+                    {
+                        "name": "Richard Tate",
+                        "gender": "male",
+                        "profession": "local business owner"
+                    }
+                  ]`},
+                {role: "user", content: article_text}
+            ],
+            temperature: 0,
+            max_tokens: 2000
+        });
+
+        console.log('Response data: ', response.data);
+        console.log('Message content:', response.data.choices[0].message);
+
+        // Parse the response from GPT-3 into a list of dictionaries
+        let quotedIndividuals = JSON.parse(response.data.choices[0].message.content);
+
+        for (let individual of quotedIndividuals) {
+            // Process each individual's name, gender, and role separately
+            let name = individual.name;
+            let gender = individual.gender;
+            let role = individual.role;
+
+            perspectives_data.push({
+                name: name,
+                gender: gender,
+                role: role
+            });
+        }
+
+    } catch (e) {
+        console.log(e);
+    }
+
+    console.log('Perspectives data: ', perspectives_data);
+    res.json(perspectives_data);
+});
+
 app.post('/scrape', async (req, res) => {
+
     let job_title = req.body.job_title;
 
     const getData = async () => {
         let employees_data = [];
         try {
-            const url = `https://www.googleapis.com/customsearch/v1/siterestrict?key=${API_KEY}&cx=f14c5df87642c4566&q=site:uk.linkedin.com/in%20${job_title}%20(her%20OR%20she)&num=10`;
+            const url = `https://www.googleapis.com/customsearch/v1/siterestrict?key=${GOOGLE_KEY}&cx=f14c5df87642c4566&q=site:uk.linkedin.com/in%20${job_title}%20(her%20OR%20she)&num=10`;
 
             const response = await unirest.get(url);
+            console.log("Profile response: ", response)
 
             const items = response.body.items;
-            console.log(items);
+            console.log("Profiles found: ", items);
 
             let count = 0;
             for (let item of items) {
@@ -90,7 +161,7 @@ app.post('/scrape', async (req, res) => {
                 // Self-identified gender by pronouns takes precedence all of the above
                 let identifyMale = (name.toLowerCase()).includes('(he/him)') | (name.toLowerCase()).includes('(he / him)');
                 let identifyFemale = (name.toLowerCase()).includes('(she/her)') | (name.toLowerCase()).includes('(she / her)');
-                let identifyNeutral = (name.toLowerCase()).includes('(they/them)') | (name.toLowerCase()).includes('(they / them)');
+                let identifyNeutral = (name.toLowerCase()).includes('(they/them)') | (name.toLowerCase()).includes('(they / them)') | (name.toLowerCase()).includes('(ze/hir/hirs)') | (name.toLowerCase()).includes('(ey/em/eir)');
 
                 if (identifyMale) {
                     gender = 'male';
@@ -101,7 +172,7 @@ app.post('/scrape', async (req, res) => {
                 }
 
                 if (identifyNeutral) {
-                    gender = 'neutral';
+                    gender = 'non-binary';
                 }
 
                 console.log('Gender: ', gender);
