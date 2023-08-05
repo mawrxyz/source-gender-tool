70aa4ba624570768499fd53e05cf5b0547a32ed3
Melissa Zhu <zhum17@cardiff.ac.uk>
2023-08-05 19:00:10 +0100

    Tweaked prompt to account for nonbinary people

--- app.js
+++ app.js
@@ -61,11 +61,11 @@ app.post('/detect', async (req, res) => {
                 
                 **IMPORTANT: Only include individuals who are providing supplementary comments or perspectives who could be replaced by others of similar background, experiences or expertise. Exclude individuals who are the main subject(s) of the news article. Also, exclude individuals who are only mentioned but do not provide quotes.**
 
-                Describe each individual's connection (or "role") in broad terms that explain why their perspectives are valuable to the story. This could be due to professional expertise, personal experiences, a shared background with the subject of the story, or any other aspect that makes their perspectives unique and irreplaceable. Do not mention specific company names or overly detailed job titles, unless these details are key to the person's role in the story. 
+                Describe each individual's connection (or "role") in broad terms that explain why their perspectives are valuable to the story. This could be, for example, due to professional expertise, personal experiences or a shared background with other people mentioned in the story. Do not mention specific company names or overly detailed job titles, unless these details are key to the person's role in the story. 
                 
                 If the role is a professional one, phrase the result such that someone with a similar background or expertise could be found by searching for the role on a job site like LinkedIn, and put "yes" as the value for the key "linkedin". Otherwise, if the role is highly personal such as the relative of the main subject or a resident of a city, put "no" for the key "linkedin". 
                 
-                State the individual's gender based on pronouns or honorifics used in the text. If no clear indication is given, make an educated guess based on the name or other contextual clues. If it is really ambiguous, such as if the name is gender neutral and there are no pronouns or honorifics used, just state the gender as "unknown". Briefly provide reasons for your determination of the person's gender.
+                State the individual's gender based on pronouns or honorifics used in the text. If no clear indication is given, make an educated guess based on the name or other contextual clues. If it is ambiguous, such as if the name is gender neutral and there are no pronouns or honorifics used, state the gender as "unknown". Briefly provide reasons for your determination of the person's gender.
 
                 Extract the quotes that are used, with each line containing a direct or indirect quote presented as a list item with the exact wording used in the text. There must be at least one quote for each individual included. Otherwise, omit that individual. 
                 
@@ -77,7 +77,7 @@ app.post('/detect', async (req, res) => {
                 "reasons": "Jane is a common female name. The honorific "Ms" and the pronoun "she" are also used.",
                 "role": "Senior political analyst at a think tank",
                 "linkedin": "yes",
-                "quotes": "<ul><li>Jane Doe, a senior political analyst at US think tank Think Politics, said that this was a 'highly concerning' situation.</li><li>'It is hard to say which way this will go. We shall wait and see.'</li><li>Ms Doe added that she did not think the government should make any rash moves.</li></ul>"
+                "quotes": "<ul><li>Jane Doe, a senior political analyst at US think tank Think Politics, said this was a 'highly concerning' situation.</li><li>'It is hard to say which way this will go.'</li></ul>"
                 },
                 {
                 "name": "Robin Doe",
@@ -85,14 +85,14 @@ app.post('/detect', async (req, res) => {
                 "reasons": "Robin is a unisex name, but the use of the pronoun "he" indicates this source is likely to be male.",
                 "role": "Resident of Cardiff city",
                 "linkedin": "no",
-                "quotes": "<ul><li>Cardiff resident Robin Doe said that he largely supported the government's policies regarding sustainable energy.</li></ul>"
+                "quotes": "<ul><li>Cardiff resident Robin Doe said he largely supported the city's policies regarding sustainable energy.</li></ul>"
                 },
                 "name": "Alex Tan",
                 "gender": "Unknown",
-                "reasons": "Although Alex is more often associated with men, it can also be a female name. There is not enough other information to determine the gender of this source.",
+                "reasons": "Although Alex is more often associated with men, it can also be a female name. "They" as a singular pronoun is often used by nonbinary people.",
                 "role": "Defence lawyer",
                 "linkedin": "no",
-                "quotes": "<ul><li>'My client is innocent, and we will shortly provide new evidence that will prove it,' her lawyer Alex Tan said.</li></ul>"
+                "quotes": "<ul><li>'My client is innocent, and we will shortly provide new evidence that will prove it,' her lawyer Alex Tan said. They added that the accused looked forward to seeing her family again.</li></ul>"
                 }
                 ]`},
                 {role: "user", content: article_text}
